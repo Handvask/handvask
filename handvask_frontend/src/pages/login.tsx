cf5d0ec373a369b1bfc62390bcd176932135e3ac
morten <morten>
2022-12-01 21:43:08 +0100

    det skulle gerne virke nu

--- handvask_frontend/src/pages/login.tsx
+++ handvask_frontend/src/pages/login.tsx
@@ -96,7 +96,7 @@ export default function Login() {
                   Login
                 </AsyncBtn>
                 <Link href="/register" className="mt-2">
-                  I don't have an account yet man
+                  I don't have an account yet
                 </Link>
               </div>
             </form>
