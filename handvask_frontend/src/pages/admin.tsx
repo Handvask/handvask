bb6fe67fe6ce830a7dc5774d9531ee1c49227f8a
Viktor Bjerg Nodstrøm <vinor19@student.sdu.dk>
2022-12-15 14:47:13 +0100

    updated color for promote button to green

--- handvask_frontend/src/pages/admin.tsx
+++ handvask_frontend/src/pages/admin.tsx
@@ -159,7 +159,7 @@ export default function Admin() {
                       <td>
                         <div className="btn-group btn-group-sm">
                           <AsyncBtn
-                            kind="danger"
+                            kind="success"
                             className=""
                             loading={deletingUser}
                             outline
